5689e739deec5755de04de7b58369a9fae23c6ba
bwhatnall <58062007+bwhatnall@users.noreply.github.com>
2020-04-15 18:58:03 +1200

    background color and internal linking

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("color");
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  
+    document.body.style.background = bgColor;
+    }
+
+button.addEventListener('click', function(){
+    random_bg_color();
+    })
